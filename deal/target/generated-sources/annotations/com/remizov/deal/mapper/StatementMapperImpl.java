b8263c5568747ae4913f823c5005fb01ae35ca92
naymond17 <remizovtimur@gmail.com>
2026-04-02 01:12:14 +0400

    pom.xml

---
+++ deal/target/generated-sources/annotations/com/remizov/deal/mapper/StatementMapperImpl.java
@@ -0,0 +1,35 @@
+package com.remizov.deal.mapper;
+
+import com.remizov.deal.entity.Client;
+import com.remizov.deal.entity.Statement;
+import com.remizov.deal.enums.ApplicationStatus;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-04-02T01:09:35+0400",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.10 (Microsoft)"
+)
+@Component
+public class StatementMapperImpl extends StatementMapper {
+
+    @Override
+    public Statement toStatement(Client client) {
+        if ( client == null ) {
+            return null;
+        }
+
+        Statement statement = new Statement();
+
+        statement.setClient( client );
+
+        statement.setStatus( ApplicationStatus.PREAPPROVAL );
+        statement.setCreationDate( LocalDateTime.now() );
+        statement.setStatusHistory( new ArrayList<>() );
+
+        return statement;
+    }
+}
